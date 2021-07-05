c60fa8991e6f99eaff789a6065b903a402eee6fe
Antonio Sitoe <antoniositoehl@gmail.com>
2021-07-05 17:02:44 +0200

    05-07
    pratica de assync e await

---
+++ 3-10-flexblog-footer_arquivos/flexblog/index.js
@@ -0,0 +1,26 @@
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((json) => show(json));
+
+const show = (json) => {
+  const qualidade = document.querySelector(".qualidade-item");
+  const qualidadeBody = document.querySelector("#qualidade");
+  let p = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, qui quos accusantium modi ea accusamus, natus soluta quisquam ratione quaerat exercitationem culpa sunt ducimus corrupti aliquam quis numquam debitis veritatis?`;
+
+  for (let item of json) {
+    const clone = qualidade.cloneNode(true);
+    const nome = (clone.querySelector("h2").innerText = item.title);
+    const foto = clone.querySelector("img");
+    // foto.setAttribute("src", faq[item].url);
+    // const empresa = (clone.querySelector(".em").innerText = item.empresa);
+    // const salario = (clone.querySelector(".sal").innerText = item.salario);
+    const post = item.body === undefined ? p : item.body;
+    const descricao = (clone.querySelector(".descricao").innerText = post);
+    // const email = (clone.querySelector(".email").innerText = item.email);
+
+    qualidadeBody.appendChild(clone);
+  }
+
+  qualidade.remove();
+};
